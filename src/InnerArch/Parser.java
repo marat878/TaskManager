bb73391406ab0effc433f2f44ec5006e446ce260
marat878 <marat878@mail.ru>
2014-10-04 11:13:50 +0300

    Ruzil

--- src/InnerArch/Parser.java
+++ src/InnerArch/Parser.java
@@ -1,8 +1,80 @@
 package InnerArch;
 
+import java.util.Date;
+
 /**
- * Created by Marat on 30.09.2014.
+ * Created by Ruzil on 04.10.2014.
  */
 public class Parser {
+    private SystemError errorCode;
     public TaskList cmdList;
+
+    Parser()
+    {
+        cmdList = new TaskList();
+    }
+
+    private void AddNotify(NotifyType type, String command, Date time, String title) {
+        cmdList.Add(new Notify(type, command, time, title));
+    }
+
+    private void AddAction(ActionType type, String command, Date time){
+        cmdList.Add(new Action(type, command, time));
+    }
+
+    public int ParseString(String str) {
+        String[] temp = str.split(";");
+
+        Date time = null;
+        //String title, command;
+        NotifyType nt;
+        ActionType at;
+
+        if( temp.length < 4 ) {
+            errorCode = SystemError.seWrongLineFormat;
+            return -1;
+        }
+
+        if(temp[0].equals("N")) {
+            if(temp[1].equals("W"))
+                nt = NotifyType.ntWarning;
+            else
+            if(temp[1].equals("I"))
+                nt = NotifyType.ntInfo;
+            else {
+                errorCode = SystemError.seUknownNotifyType;
+                return -1;
+            }
+
+            AddNotify( nt, temp[2], time, null );
+
+        }
+        else
+        if(temp[0].equals("A")) {
+            if(temp[1].equals("S"))
+                at = ActionType.atStartProc;
+            else
+            if(temp[1].equals("K"))
+                at = ActionType.atKillProc;
+            else {
+                errorCode = SystemError.seUnknownActionType;
+                return -1;
+            }
+
+            AddAction( at, temp[2], time );
+        }
+        else
+        {
+            errorCode = SystemError.seWrongLineHeader;
+            return -1;
+        }
+
+        errorCode = SystemError.seNone;
+        return 0;
+    }
+
+    public SystemError GetErrorCode()
+    {
+        return errorCode;
+    }
 }
---
+++ src/InnerArch/SystemError.java
@@ -0,0 +1,12 @@
+package InnerArch;
+
+/**
+ * Created by Ruzil on 04.10.2014.
+ */
+public enum SystemError {
+    seNone,
+    seUknownNotifyType,
+    seUnknownActionType,
+    seWrongLineFormat,
+    seWrongLineHeader
+}
